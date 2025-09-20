84ae1227ec189b3d911ccfce3b87ced8133d6749
srikanth159 <150225151+srikanth159@users.noreply.github.com>
2025-09-20 16:29:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-const launchButton = document.getElementById('launchButton');
-const timerElement = document.getElementById('timer');
+ const timerElement = document.getElementById('timer');
 const daysElement = document.getElementById('days-number');
 const hoursElement = document.getElementById('hours-number');
 const minutesElement = document.getElementById('minutes-number');
 const secondsElement = document.getElementById('seconds-number');
 
-var countDownDate = new Date("Apr 28, 2026 10:30:00").getTime();
+var countDownDate = new Date("sep 20, 2025 16:09:00").getTime();
 
-// Update the count down every 1 second
-const x = setInterval(function() {
-
-  // Get today's date and time
+const x = setInterval(function () {
   const now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
   const distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
+
+  if (distance < 0) {
+    clearInterval(x);
+    timerElement.textContent = "Now the Game is Live"; // Show EXPIRED in place of countdown
+    return;
+  }
+
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
- daysElement.textContent = days;
-         hoursElement.textContent = hours;
-        minutesElement.textContent = minutes;
-         secondsElement.textContent = seconds;
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
+
+  daysElement.textContent = days;
+  hoursElement.textContent = hours;
+  minutesElement.textContent = minutes;
+  secondsElement.textContent = seconds;
 }, 1000);
+
+
